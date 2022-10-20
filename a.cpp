a3bd42e38ffcb06eefb9a5b8eddd07f3d7b553d8
xubu <641706106@qq.com>
2022-10-20 23:14:14 +0100

    10.20

--- a.cpp
+++ a.cpp
@@ -15,58 +15,13 @@ struct TreeNode {
 
 class Solution {
 public:
-    vector<int> merge(vector<int> &v1, vector<int> &v2, vector<int> &nums) {
-        vector<int> ret;
-        int p = 0, q = 0;
-        while (p < v1.size() and q < v2.size()) {
-            if (nums[v1[p]] < nums[v2[q]]) {
-                ret.emplace_back(v1[p]);
-                p++;
-            } else {
-                ret.emplace_back(v2[q]);
-                q++;
-            }
-        }
-        while (p < v1.size()) {
-            ret.emplace_back(v1[p]);
-            p++;
-        }
-        while (q < v2.size()) {
-            ret.emplace_back(v2[q]);
-            q++;
-        }
-        return ret;
+    //given the value of the sum of component, the return the max edge that can be deleted
+    int sub(int value,vector<int>& nums, vector<vector<int>>& edges){
+
     }
 
-    vector<int> mergeSort(vector<int> &indexs, vector<int> &nums, int start, int end) {
-        if (indexs.size() <= 1) {
-            return indexs;
-        }
-        vector<int> l;
-        int mid = start + ((end - start) >> 1);
-        for (int i = 0; i < mid; ++i) {
-            l.emplace_back(indexs[i]);
-        }
-        l=mergeSort(l, nums, start, mid);
-        vector<int> r;
-        for (int i = mid; i < end; ++i) {
-            r.emplace_back(indexs[i]);
-        }
-        r=mergeSort(r, nums, mid, end);;
-        return merge(l, r, nums);
-    }
+    int componentValue(vector<int>& nums, vector<vector<int>>& edges) {
 
-    vector<int> smallerNumbersThanCurrent(vector<int> &nums) {
-        vector<int> indexs;
-        for (int i = 0; i < nums.size(); ++i) {
-            indexs.emplace_back(i);
-        }
-        vector<int> ans(nums.size());
-        indexs = mergeSort(indexs, nums, 0, nums.size());
-        for (int i = 0; i < ans.size(); ++i) {
-            ans[indexs[i]]=i;
-        }
-        return ans;
     }
 };
 
