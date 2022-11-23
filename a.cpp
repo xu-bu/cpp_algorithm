957d2116a7a5d12e60100b8bb4ef03fa6964f56f
xubu <641706106@qq.com>
2022-11-23 19:49:51 +0000

    11.23

--- a.cpp
+++ a.cpp
@@ -16,39 +16,64 @@ struct TreeNode {
 
 class Solution {
 public:
-    //787 Bellman-Ford algorithm. This algorithm is used to calculate the shortest path from s to d in a directed graph with negative weight but with no negative cycle. Time complexity O(mn), space complexity O(n**2).
-    int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int k) {
-//        int rows=flights.size(),cols=flights[0].size();
-        int edgeCount = flights.size();
-        int inf = 100001;
-        vector<vector<int>> edges(n, vector<int>(n, inf));
-        // matrix[v][n] means the shortest path start from v to dst within n arcs
-        vector<vector<int>> matrix(n, vector<int>(n));
-        for (auto &each: flights) {
-            edges[each[0]][each[1]] = each[2];
-        }
-        for (int arcs = 1; arcs < edgeCount; ++arcs) {
-            for (int v = 0; v < n; ++v) {
-                matrix[v][arcs] = matrix[v][arcs - 1];
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+        vector<bool> visit(n);
+        function<bool(int)> checkTree=[&](int deleteEdge){
+            map<int,int> degree;
+            map<int,unordered_set<int>> node2edges;
+            for (int i = 0; i < n; ++i) {
+                if(i!=deleteEdge){
+                    degree[edges[i][0]]++;
+                    degree[edges[i][1]]++;
+                    node2edges[edges[i][0]].emplace(edges[i][1]);
+                    node2edges[edges[i][1]].emplace(edges[i][0]);
+                }
             }
-            for (auto &each: flights) {
-                matrix[each[0]][arcs] = min(matrix[each[0]][arcs - 1], each[2] + matrix[each[1]][arcs - 1]);
+            stack<int> deleteNodes;
+            for(auto &each:degree){
+                if(each.second==1){
+                    deleteNodes.push(each.first);
+                }
+            }
+            while(!deleteNodes.empty()){
+                int node=deleteNodes.top();
+                deleteNodes.pop();
+                visit[node]=true;
+                for(auto &each:node2edges[node]){
+                    degree[each]--;
+                    if(degree[each]==1){
+                        deleteNodes.emplace(each);
+                    }
+                }
+            }
+            for(auto each:visit){
+                if(!each){
+                    return false;
+                }
+            }
+            return true;
+        };
+        for (int i = n-1; i >=0; ++i) {
+            if(checkTree(i)){
+                return edges[i];
             }
         }
-        return matrix[src][edgeCount - 1];
+        return vector<int>{};
     }
 };
-    int main() {
-        string word = "ab";
-        string prefix = "app";
-        vector<int> nums = {6, 2, 2, 2, 6};
-        vector<vector<int>> edges = {{0, 1},
-                                     {1, 2},
-                                     {1, 3},
-                                     {3, 4}};
-        Solution solution = Solution();
-        cout << solution.componentValue(nums, edges);
 
-    }
+int main() {
+    string word = "ab";
+    string prefix = "app";
+    vector<int> nums = {6, 2, 2, 2, 6};
+    vector<vector<int>> edges = {{0, 1},
+                                 {1, 2},
+                                 {1, 3},
+                                 {3, 4}};
+    Solution solution = Solution();
+    cout << solution.componentValue(nums, edges);
+
+}
 
 
